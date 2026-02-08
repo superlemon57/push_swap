20b1c5a9ab4fe5e56fd4589b585527edb7cd9650
Muriel Ledda <mledda@c1r7p3.42nice.fr>
2026-02-08 16:32:15 +0100

    avecoperations

---
+++ reverse_rotate.c
@@ -0,0 +1,42 @@
+#include "push_swap.h"
+
+void    reverse_rotate(StackElement **s)
+{
+    StackElement *prev;
+    StackElement *last;
+
+    if (!s || !*s || !(*s)->next)
+        return;
+
+    prev = NULL;
+    last = *s;
+
+    while (last->next)
+    {
+        prev = last;
+        last = last->next;
+    }
+
+    prev->next = NULL;
+    last->next = *s;
+    *s = last;
+}
+
+void    rra(StackElement **a)
+{
+    reverse_rotate(a);
+    write(1, "rra\n", 4);
+}
+
+void    rrb(StackElement **b)
+{
+    reverse_rotate(b);
+    write(1, "rrb\n", 4);
+}
+
+void    rrr(StackElement **a, StackElement **b)
+{
+    reverse_rotate(a);
+    reverse_rotate(b);
+    write(1, "rrr\n", 4);
+}
