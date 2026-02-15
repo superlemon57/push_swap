35087e5b78c7198312606285ae25a6d3ad9f52fe
Muriel <mledda@student.42nice.fr>
2026-02-15 21:24:10 +0100

    indexation faite

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -3,52 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   reverse_rotate.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlopez <tlopez@student.42nice.fr>          +#+  +:+       +#+        */
+/*   By: mledda <mledda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 17:29:24 by tlopez            #+#    #+#             */
-/*   Updated: 2026/02/11 17:29:24 by tlopez           ###   ########.fr       */
+/*   Updated: 2026/02/15 16:36:54 by mledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    reverse_rotate(StackElement **s)
+void	reverse_rotate(StackElement **s)
 {
-    StackElement *prev;
-    StackElement *last;
+	StackElement	*prev;
+	StackElement	*last;
 
-    if (!s || !*s || !(*s)->next)
-        return;
+	if (!s || !*s || !(*s)->next)
+		return ;
 
-    prev = NULL;
-    last = *s;
+	prev = NULL;
+	last = *s;
 
-    while (last->next)
-    {
-        prev = last;
-        last = last->next;
-    }
+	while (last->next)
+	{
+		prev = last;
+		last = last->next;
+	}
 
-    prev->next = NULL;
-    last->next = *s;
-    *s = last;
+	prev->next = NULL;
+	last->next = *s;
+	*s = last;
 }
 
-void    rra(StackElement **a)
+void	rra(StackElement **a)
 {
-    reverse_rotate(a);
-    write(1, "rra\n", 4);
+	reverse_rotate(a);
+	write(1, "rra\n", 4);
 }
 
-void    rrb(StackElement **b)
+void	rrb(StackElement **b)
 {
-    reverse_rotate(b);
-    write(1, "rrb\n", 4);
+	reverse_rotate(b);
+	write(1, "rrb\n", 4);
 }
 
-void    rrr(StackElement **a, StackElement **b)
+void	rrr(StackElement **a, StackElement **b)
 {
-    reverse_rotate(a);
-    reverse_rotate(b);
-    write(1, "rrr\n", 4);
+	reverse_rotate(a);
+	reverse_rotate(b);
+	write(1, "rrr\n", 4);
 }
--- simple_sort.c
+++ simple_sort.c
@@ -3,145 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   simple_sort.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlopez <tlopez@student.42nice.fr>          +#+  +:+       +#+        */
+/*   By: mledda <mledda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 17:29:12 by tlopez            #+#    #+#             */
-/*   Updated: 2026/02/11 17:29:12 by tlopez           ###   ########.fr       */
+/*   Updated: 2026/02/15 16:34:18 by mledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-typedef struct s_count_operations
+int	find_pos(StackElement *a, int value)
 {
-        int     count_pa;
-        int     count_pb;
-        int     count_sa;
-        int     count_sb;
-        int     count_ss;
-        int     count_ra;
-        int     count_rb;
-        int     count_rr;
-        int     count_rra;
-        int     count_rrb;
-        int     count_rrr;
-} t_count_operations;
+	int	i;
 
-int stack_size(StackElement *s)
-{
-    int count = 0;
-
-    while (s)
-    {
-        count++;
-        s = s->next;
-    }
-    return (count);
+	i = 0;
+	while (a)
+	{
+		if (a->value == value)
+			return (i);
+		a = a->next;
+		i++;
+	}
+	return (-1);
 }
 
-int is_sorted(StackElement *a)
+void	bring_to_top(StackElement **a, int pos)
 {
-    while (a && a->next)
-    {
-        if (a->value > a->next->value)
-            return (0);
-        a = a->next;
-    }
-    return (1);
+	int	size;
+
+	size = stack_size(*a);
+
+	if (pos <= size / 2)
+	{
+		while (pos-- > 0)
+			ra(a);
+	}
+	else
+	{
+		while (pos++ < size)
+			rra(a);
+	}
 }
 
-int find_min(StackElement *a)
+void	sort_three(StackElement **a)
 {
-    int min;
+	int	x;
+	int	y;
+	int	z;
 
-    min = a->value;
-    while (a)
-    {
-        if (a->value < min)
-            min = a->value;
-        a = a->next;
-    }
-    return (min);
+	x = (*a)->value;
+	y = (*a)->next->value;
+	z = (*a)->next->next->value;
+
+	if (x > y && y < z && x < z)
+		sa(a);
+	else if (x > y && y > z)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if (x > y && y < z && x > z)
+		ra(a);
+	else if (x < y && y > z && x < z)
+	{
+		sa(a);
+		ra(a);
+	}
+	else if (x < y && y > z && x > z)
+		rra(a);
 }
 
-int find_pos(StackElement *a, int value)
+void	simple_sort(StackElement **a, StackElement **b)
 {
-    int i = 0;
+	int	min;
+	int	pos;
 
-    while (a)
-    {
-        if (a->value == value)
-            return (i);
-        a = a->next;
-        i++;
-    }
-    return (-1);
-}
+	if (is_sorted(*a))
+		return ;
+	while (stack_size(*a) > 3)
+	{
+		min = find_min(*a);
+		pos = find_pos(*a, min);
 
-void bring_to_top(StackElement **a, int pos)
-{
-    int size;
-
-    size = stack_size(*a);
-
-    if (pos <= size / 2)
-    {
-        while (pos-- > 0)
-            ra(a);
-    }
-    else
-    {
-        while (pos++ < size)
-            rra(a);
-    }
-}
-
-
-void sort_three(StackElement **a)
-{
-    int x;
-    int y;
-    int z;
-
-    x = (*a)->value;
-    y = (*a)->next->value;
-    z = (*a)->next->next->value;
-
-    if (x > y && y < z && x < z)
-        sa(a);
-    else if (x > y && y > z)
-    {
-        sa(a);
-        rra(a);
-    }
-    else if (x > y && y < z && x > z)
-        ra(a);
-    else if (x < y && y > z && x < z)
-    {
-        sa(a);
-        ra(a);
-    }
-    else if (x < y && y > z && x > z)
-        rra(a);
-}
-
-
-void simple_sort(StackElement **a, StackElement **b)
-{
-    int min;
-    int pos;
-
-    if (is_sorted(*a))
-        return ;
-    while (stack_size(*a) > 3)
-    {
-        min = find_min(*a);
-        pos = find_pos(*a, min);
-
-        bring_to_top(a, pos);
-        pb(a, b);
-    }
-    sort_three(a);
-    while (*b)
-        pa(a, b);
+		bring_to_top(a, pos);
+		pb(a, b);
+	}
+	sort_three(a);
+	while (*b)
+		pa(a, b);
 }
